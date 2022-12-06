e96b0bf975a50cac064d459227c571ab8bf40c5e
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-12-06 14:36:42 +0100

    yeet

---
+++ cadeaustress/index.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for _ in range(n):
+    [a_x, a_y, name] = input().split()
+    x = int(a_x)
+    y = int(a_y)
--- voorspelbaar/index.py
+++ voorspelbaar/index.py
@@ -1,19 +1,20 @@
 n = int(input())
 
-strng = input().split()
-toremove = []
 # print(strng)
 
-while True:
-    current = input()
+for _ in range(n):
+    strng = input().split()
+    toremove = []
+    while True:
+        current = input()
 
-    if current == "wat drinkt de nieuwe viaan?":
-        break
-    else:
-        [name, item] = current.split(" drinkt ")
-        # print(name, item)
-        toremove.append(item)
-        # strng.remove(item)
-        # strng = strng.replace(item, "")
+        if current == "wat drinkt de nieuwe viaan?":
+            break
+        else:
+            [name, item] = current.split(" drinkt ")
+            # print(name, item)
+            toremove.append(item)
+            # strng.remove(item)
+            # strng = strng.replace(item, "")
 
-print(" ".join(filter(lambda x: x not in toremove, strng)))
+    print(" ".join(filter(lambda x: x not in toremove, strng)))
